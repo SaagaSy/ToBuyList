02197cbc3a0de51c7cd889d815dac1d332d25e68
your_github_sasy <saaga.syrjanen@hotmail.com>
2026-02-11 12:23:36 +0100

    Initial commit

---
+++ src/components/ToBuyList.jsx
@@ -0,0 +1,14 @@
+export default function ToBuyList() {
+  const day = "Friday";
+
+  return (
+    <>
+      <h1>Needed to buy on {day}:</h1>
+      <ul>
+        <li>Flowers</li>
+        <li>Shampoo</li>
+        <li>Toothpaste</li>
+      </ul>
+    </>
+  );
+}
