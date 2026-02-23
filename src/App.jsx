811d1c80ce8278aba9283934cd8a86e47b5dcd62
your_github_sasy <saaga.syrjanen@hotmail.com>
2026-02-23 23:05:58 +0100

    routing + established global style with styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,54 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import Parse from "./services/parse";
 import "./App.css";
+import NavBar from "./components/NavBar";
 import ToBuyList from "./pages/ToBuyList";
+import Welcome from "./pages/Welcome";
+import Auth from "./pages/Auth";
+import OverviewLists from "./pages/OverviewLists";
+import { createGlobalStyle } from "styled-components";
 
 // Handles layout, routing and structure of UI
 
+// Global styles
+const GlobalStyle = createGlobalStyle`
+body {
+  background-color:#fffefb;
+  margin: 0;
+  padding: 0;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  box-sizing: border-box;
+}
+  *, *::before, *::after {
+    box-sizing: inherit;
+  } 
+`;
+
 function App() {
   // Global state for tracking if user is logged in
-  const [currentUser, setCurrentUser] = useState(null);
-
-  // Check for a logged-in user when app opens:
-  useEffect(() => {
-    const user = Parse.User.current();
-    if (user) {
-      setCurrentUser(user);
-    }
-  }, []);
+  const [currentUser, setCurrentUser] = useState(Parse.User.current());
 
   return (
     <BrowserRouter>
+      <GlobalStyle />
       <NavBar currentUser={currentUser} />
 
       <div className="content">
         <Routes>
-          <Route path="/" element={<Welcome />} />
+          <Route
+            path="/"
+            element={
+              currentUser ? <Navigate to="/lists" replace /> : <Welcome />
+            }
+          />
+
           <Route
             path="/auth"
             element={<Auth setCurrentUser={setCurrentUser} />}
           />
-          <Route path="/list" element={<ToBuyList />} />
+          <Route path="/lists" element={<OverviewLists />} />
+          <Route path="/list/:listId" element={<ToBuyList />} />
         </Routes>
       </div>
     </BrowserRouter>
